401de85eab24feb16b474ff0528fac0a4a5bdde6
Molrn <76754000+Molrn@users.noreply.github.com>
2026-06-25 14:00:16 +0200

    Add frame sandbox

---
+++ core/src/frame_sandbox.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import toga
+
+active_switch_index = 0
+
+
+def build(app):
+    box = toga.Box(direction="column", margin=20, width=400, height=60)
+    open_data_path_button = toga.Button("Open")
+    edit_data_path_button = toga.Button("Edit")
+    data_path_input = toga.TextInput(
+        value=str(Path().absolute()),
+        readonly=True,
+        flex=1,
+    )
+    data_frame_content = toga.Box(
+        direction="column",
+        children=[
+            data_path_input,
+            toga.Box(
+                direction="row",
+                children=[
+                    open_data_path_button,
+                    edit_data_path_button,
+                ],
+                gap=10,
+            ),
+        ],
+        background_color="red",
+        margin=(0, 10),
+        align_items="center",
+        flex=1,
+    )
+    frame = toga.Frame(
+        title="Data path",
+        content=data_frame_content,
+        flex=1,
+        font_weight="bold",
+        font_size=9,
+    )
+
+    def increase_height(widget):
+        box.style.height += 5
+
+    def increase_font(widget):
+        frame.style.font_size += 1
+
+    box.add(frame)
+    box.add(
+        toga.Button("Height", on_press=increase_height),
+        toga.Button("Font", on_press=increase_font),
+    )
+
+    return box
+
+
+def main():
+    return toga.App(
+        "First App",
+        "org.beeware.toga.examples.tutorial",
+        startup=build,
+    )
+
+
+if __name__ == "__main__":
+    main().main_loop()
